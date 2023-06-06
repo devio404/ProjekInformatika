c3a5973b97b6b3ec9fec247295830a3505177f2e
Dio Rizqi <dev404@localhost.localdomain>
2023-06-06 10:28:48 +0700

    add contact form

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+  }
